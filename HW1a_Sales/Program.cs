8b135841d763175c018ae06e1e49919134e4f4d3
Kimberly Nunez Noris <Kimberly.Nunez.Noris-1@ou.edu>
2025-09-12 10:00:00 -0500

    Completed 9/8

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,10 +1,11 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Kimberly Nunez-Noris
+// Did you seek help ? If yes, specify the helper or web link here: Dr.Xuang Feng
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,38 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            double amount;
+            double price;
+
+            double subtotal;
+            double saletax;
+            double bill;
+
+            const double tax = .085;
+
+            string amountAsString;
+            string priceAsString;
+
+
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            string product = Console.ReadLine();
+            Console.WriteLine("How many " + product + "'s do you want to buy?");
+            amountAsString = Console.ReadLine();
+            Console.WriteLine("What is the price for each " + product + "?");
+            priceAsString = Console.ReadLine();
+
+            amount = Convert.ToDouble(amountAsString);
+            price = Convert.ToDouble(priceAsString);
+
+            subtotal = amount * price;
+            saletax = subtotal * tax;
+            bill = subtotal + saletax;
+
+            Console.WriteLine($"Your subtotal for your bill is {subtotal.ToString("C2")}");
+            Console.WriteLine($"Your sale tax for your bill is {saletax.ToString("C2")}");
+            Console.WriteLine($"Your total for your bill is {bill.ToString("C2")}");
+            Console.ReadKey();
+
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,84 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            double part;
+            double hmwrk;
+            double e1;
+            double e2;
+            double e3;
+
+            const double p = 0.0015;
+            const double h = 0.0020;
+            const double exam1 = 0.0015;
+            const double exam2 = 0.0025;
+            const double exam3 = 0.0025;
+
+            double sum;
+
+            string partAsString;
+            string hmwrkAsString;
+            string e1AsString;
+            string e2AsString;
+            string e3AsString;
+
+
+
+            Console.WriteLine("What is your first name?");
+
+            string fname = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+
+            string lname = Console.ReadLine();
+
+            Console.WriteLine("What is your student id?");
+
+            var studentid = Console.ReadLine();
+
+            
+
+            Console.WriteLine("What is your overall percentage grade for participations?"); // 15%
+
+            partAsString = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?"); // 20%
+
+            hmwrkAsString = Console.ReadLine();
+
+
+            Console.WriteLine("What is your overall percentage grade for Exam 1?"); // 15%
+
+            e1AsString = Console.ReadLine();
+
+
+            Console.WriteLine("What is your overall percentage grade for Exam 2?"); // 25%
+
+            e2AsString = Console.ReadLine();
+
+
+            Console.WriteLine("What is your overall percentage grade for Exam 3"); // 25%
+
+            e3AsString = Console.ReadLine();
+
+            part = Convert.ToDouble(partAsString);
+            hmwrk = Convert.ToDouble(hmwrkAsString);
+            e1 = Convert.ToDouble(e1AsString);
+            e2 = Convert.ToDouble(e2AsString);
+            e3 = Convert.ToDouble(e3AsString);
+
+            // const double p = .15;
+           // const double h = .20;
+           // const double exam1 = .15;
+           // const double exam2 = .25;
+           // const double exam3 = .25;
+
+            sum = (part * p) + (hmwrk * h) + (e1 * exam1) + (e2 * exam2) + (e3 * exam3);
+
+            Console.WriteLine(fname + " " + lname + " " + "(" + studentid + ") " + "your final grade is " + sum.ToString("P"));
+
+            Console.ReadKey();
+            
+
         }
     }
 }
